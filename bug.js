2388c0a6873a6ab78adf67a7e825db8ff368c54d
BugHunter-X <askzenai@gmail.com>
2025-02-23 09:00:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // ... some database query to find the user ...
+  if (!user) {
+    return res.status(404).send('User not found'); // Correct handling
+  }
+  res.send(user); 
+});
+
+//Problem: Error Handling - Incorrect use of next() in async middleware
+app.use(async (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+  next(); // Incorrect placement of next()
+});
